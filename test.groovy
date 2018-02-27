7d34244695f92157d2c63e61655e0d3842650567
Andrew Bayer <andrew.bayer@gmail.com>
2018-02-27 14:36:15 -0500

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -9,11 +9,11 @@ node('master') {
 }
 
 lib.ZKIMG = ['ZKIP', 'zookeeper:3.3.6', 1, '']
-lib.KAFKAIMG = ['KAFKAIP', 'kafka:0.8.1.1', 1, "--env \"ZK_CONNECT=${lib.IPS['ZKIP']}:2181\" "]
+lib.KAFKAIMG = ['KAFKAIP', 'kafka:0.8.1.1', 1, "--env \"ZK_CONNECT=${ -> lib.IPS['ZKIP']}:2181\" "]
 
 lib.MODULE = 'test'
 lib.MODULEOPTS = "--env MODULE=${lib.MODULE} " +
-        "--env \"${lib.MODULE.toUpperCase()}_ZK_IP=${lib.IPS['ZKIP']}\" "
+        "--env \"${lib.MODULE.toUpperCase()}_ZK_IP=${ -> lib.IPS['ZKIP']}\" "
 
 lib.ATSERVICES = [lib.ZKIMG,
                   lib.KAFKAIMG,
